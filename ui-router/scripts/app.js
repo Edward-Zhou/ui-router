8610f5b1fc17b576ce0c4de7648c31b9ecdeafab
Edward <v-tazho@hotmail.com>
2016-05-04 18:11:19 +0800

    ngrouter vs ui.router

---
+++ ui-router/scripts/app.js
@@ -0,0 +1,44 @@
+﻿/// <reference path="angular-ui-router.js" />
+var app = angular.module('_app_', ['ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('index', {
+        url: '',
+        views:{
+            '':{templateUrl:'partials/index.body.html'}
+        }
+    })
+    .state('index.red', {
+        url: '/red',
+        templateUrl:'partials/index.body.red.html'
+    })
+    .state('index.green', {
+        url: '/green',
+        templateUrl:'partials/index.body.green.html'
+    })
+    .state('index.yellow', {
+        url: '/yellow',
+        templateUrl:'partials/index.body.yellow.html'
+    })
+
+})
+.controller('navController', function ($scope, $state) {
+    $scope.navToGreen = function () { $state.transitionTo('index.green'); };
+    $scope.setUrl = function (p) { location.href = "http://localhost:18752/index-ui-router.html#/"+p };
+
+});
+
+//app.config(function ($stateProvider, $urlRouterProvider) {
+//    $stateProvider.state('index', {
+//        url: '',
+//        views:{
+//            '': { templateUrl: 'partials/index.html' },
+//            '@index': { templateUrl: 'partials/index.body.html' }
+//        }
+        
+//    })
+//    .state('index.red', {
+//        url: '/red',
+//        templateUrl: 'partials/index.body.red.html'
+//    })
+//});
---
+++ ui-router/scripts/ngapp.js
@@ -0,0 +1,33 @@
+﻿/// <reference path="angular-ui-router.js" />
+var app = angular.module('_app_', ['ngRoute']);
+
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/red', {
+        templateUrl:'partials/index.body.red.html'
+    })
+    .when('/green', {
+        templateUrl: 'partials/index.body.green.html'
+    })
+    .when('/yellow', {
+        templateUrl: 'partials/index.body.yellow.html'
+    })
+})
+    .controller('navController', function ($scope) {
+    $scope.setUrl = function (p) { location.href = "http://localhost:18752/index-ng-router.html#/" + p };
+
+});
+
+//app.config(function ($stateProvider, $urlRouterProvider) {
+//    $stateProvider.state('index', {
+//        url: '',
+//        views:{
+//            '': { templateUrl: 'partials/index.html' },
+//            '@index': { templateUrl: 'partials/index.body.html' }
+//        }
+        
+//    })
+//    .state('index.red', {
+//        url: '/red',
+//        templateUrl: 'partials/index.body.red.html'
+//    })
+//});
